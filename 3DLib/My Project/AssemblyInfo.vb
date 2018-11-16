82b1a5d3898a3d9e5364bd23669a22f6fa23ce6d
bezurog <bezurog@gmail.com>
2018-11-16 18:16:40 +0000

    removed nuget dependencies

--- 3DLib/My Project/AssemblyInfo.vb
+++ 3DLib/My Project/AssemblyInfo.vb
@@ -8,11 +8,11 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("3DLib")> 
+<Assembly: AssemblyTitle("OpenTK.Pipe")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("3DLib")> 
-<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyProduct("OpenTK.Pipe")> 
+<Assembly: AssemblyCopyright("bezurog ©  2018")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.1.0")> 
+<Assembly: AssemblyFileVersion("1.0.1.0")> 
