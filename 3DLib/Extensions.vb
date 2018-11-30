e2c7b3c9bac539ae38c59c1e6dd4435ec2f91d66
bezurog <bezurog@gmail.com>
2018-11-30 18:47:54 +0000

    writeonly mode for bitmapdata for screenshot

--- 3DLib/Extensions.vb
+++ 3DLib/Extensions.vb
@@ -13,8 +13,10 @@ Public Module Extensions
     Public Sub GrabScreenShot(ByVal glControl As GLControl, fileName As String, Optional pixelType As PixelType = PixelType.UnsignedByte)
 
             Dim bmp As Bitmap = New Bitmap(glControl.ClientRectangle.Width, glControl.ClientRectangle.Height)
-            Dim data As BitmapData = bmp.LockBits(glControl.ClientRectangle, ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb)
-            GL.ReadPixels(0, 0, glControl.Width, glControl.Height, OpenGL.PixelFormat.Bgr, OpenGL.PixelType.UnsignedByte, data.Scan0)
+            Dim data As BitmapData = bmp.LockBits(glControl.ClientRectangle, ImageLockMode.WriteOnly, 
+                                                  System.Drawing.Imaging.PixelFormat.Format24bppRgb)
+            GL.ReadPixels(0, 0, glControl.ClientRectangle.Width, glControl.ClientRectangle.Height, 
+                          OpenGL.PixelFormat.Bgr,OpenGL.PixelType.UnsignedByte, data.Scan0)
             bmp.UnlockBits(data)
             bmp.RotateFlip(RotateFlipType.RotateNoneFlipY)
             bmp.Save(fileName, ImageFormat.Png)
